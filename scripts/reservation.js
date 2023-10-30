e89df35a4e594d0b4026326a70007adb39cecdd6
ojohnson1 <ojohnson659@gmail.com>
2023-10-30 12:32:17 -0400

    in/out of seasons working

--- scripts/reservation.js
+++ scripts/reservation.js
@@ -10,55 +10,68 @@ function init() {
 
 
 
-function getRoomRate(checkInDate, queenBedRoom, kingBedRoom, twoBedRoom) {
+function getRoomRate() {
   //Get the starting values
-   queenBedRoom = document.getElementById("queenBedRoom").checked;
-   kingBedRoom = document.getElementById("kingBedRoom").checked;
-   twoBedRoom = document.getElementById("twoBedRoom").checked;
-   checkInDate = new Date (document.getElementById ("checkInDate"));
-   let numberOfNightsField=document.getElementById('numberOfNights');
-   
-   let roomRate;
-  
+  const queenBedRoom = document.getElementById("queenBedRoom").checked;
+  const kingBedRoom = document.getElementById("kingBedRoom").checked;
+  const twoBedRoom = document.getElementById("twoBedRoom").checked;
+  const checkInDate = new Date (document.getElementById ("checkInDate").value);
+
+  let roomRate=0;
+
+  let month=checkInDate.getMonth() ;
+ let summerSeason= month >=5 && month <=7
 //getting the month 
-    let month=checkInDate.getMonth().value;
 
-// these if statements determine the in/out of season rates being applied 
- if (5<=month <=7  && queenBedRoom ||  kingBedRoom) {
-     roomRate = 250
-  } else if ((queenBedRoom || kingBedRoom)) {
-     roomRate = 150
-   }
+if( summerSeason){
+  //summer rates
 
-   if (5<=month <=7 && twoBedRoom) {
-     roomRate = 350
-   } else if (twoBedRoom) {
-     roomRate = 210
-   }
+ if(queenBedRoom || kingBedRoom)
+  {
+    roomRate=250;
+  }
+  else if (twoBedRoom){
+ roomRate=350
+  }
+  else{
+    console.log("Error, type of bed not detected");
+  }
 
+}
+else {
   
+  if(queenBedRoom || kingBedRoom)
+  {
+     roomRate=150
+  }
+  else if (twoBedRoom){
+   roomRate=210
+  }
+  else{
+    console.log("Error, type of bed not detected");
+  }
 
-    //Output Fields
-  let outputOriginalRoomCost=document.getElementById("originalRoomCost");
-  let numberOfNights=Number(numberOfNightsField.value);
-  //Display Output
-  let originalRoomCost= roomRate*numberOfNights;
- outputOriginalRoomCost.value=originalRoomCost;
- 
- 
-   
 }
 
-// i need all the ids of the radio buttons and check input date
-//the check in date input
 
-//Room type rates Queen/King $250 $150 2Bedroom $350 $210
+  //  if ( summerSeason  && queenBedRoom ||  kingBedRoom) {
+  //    roomRate = 250
+  // } else if ((queenBedRoom || kingBedRoom)) {
+  //    roomRate = 150
+  //  }
 
-//Calculate the unknown values
+  //  if ( summerSeason && twoBedRoom) {
+  //    roomRate = 350
+  //  } else if (twoBedRoom) {
+  //    roomRate = 210
+  //  }
 
-/*i need to wire up the getRoomrate function to return a value based on what room they check and what month they book
- .getMonth maybe used
+    //Output Fields
+   let outputOriginalRoomCost=document.getElementById("originalRoomCost");
+  //Display Output
+  outputOriginalRoomCost.value=roomRate;
 
-//roomrate * number of nights= original room cost*/
 
-//Display the output
+
+}
+
